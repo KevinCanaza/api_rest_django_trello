8d5b57dd0a39c3dfe183503b1de20edbaf766c93
KevinCanaza <canazasangakevin@gmail.com>
2023-07-13 00:10:36 -0400

    subiendo proyecto back end base

---
+++ trello_clone_api/settings/production.py
@@ -0,0 +1,33 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'HOST': 'localhost',
+        'PORT': 5432,
+        "NAME": "db_trello",
+        'USER': 'postgres',
+        'PASSWORD': 'K123EqweV,./I!@#NQWE',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
