15b9c70603abfd2d0c2b2f2dd29fa57946f92330
KevinCanaza <102050299+KevinCanaza@users.noreply.github.com>
2023-07-17 03:12:02 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 
 echo "Building the project..."
 
-python3.10 - m pip install - r requeriments.txt
+python - m pip install - r requeriments.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manaye.py migrate -noinput
+python manage.py makemigrations --noinput
+python manaye.py migrate -noinput
 
 echo "Collect Static..."
-python3.10 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 
